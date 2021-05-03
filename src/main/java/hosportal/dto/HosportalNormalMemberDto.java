b37c466dc55919a3b6014237d23ec991bcba268d
DESKTOP-FK0507E\sua10 <sua100913@gmail.com>
2021-05-03 17:15:34 +0900

    일반회원 구성목록

---
+++ src/main/java/hosportal/dto/HosportalNormalMemberDto.java
@@ -0,0 +1,23 @@
+package hosportal.dto;
+
+import lombok.Data;
+
+@Data
+public class HosportalNormalMemberDto {
+
+	private String normalEmail;
+	private String normalPassword;
+	private String normalTel;
+	private String normalTel1;
+	private String normalTel2;
+	private String normalTel3;
+	private String normalAddress1;
+	private String normalAddress2;
+	private String normalAddress3;
+	private String normalGender;
+	private String normalName;
+	private String normalNickname;
+	private String normalBirth;
+	private String normalRegisterDate;
+	private String fileName;
+}
