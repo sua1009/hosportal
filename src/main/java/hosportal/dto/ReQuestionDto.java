c4f1bdc2b0d54dd9a22cf1ef049b843b45b6417d
DESKTOP-FK0507E\sua10 <sua100913@gmail.com>
2021-05-03 17:12:43 +0900

    문의 댓글 구성목록

---
+++ src/main/java/hosportal/dto/ReQuestionDto.java
@@ -0,0 +1,17 @@
+package hosportal.dto;
+
+import lombok.Data;
+
+@Data
+public class ReQuestionDto {
+	
+		private int reQuestionNo;
+		private String writer;
+		private String reQuestionContent;
+		private String reQuestionCreatedDate;
+		private String questionNo;
+		private String hospitalId;
+		private String normalEmail;
+	
+
+}
