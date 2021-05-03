9a2da9f2efa0b79b23b134c8636a1bbc6fd55671
DESKTOP-FK0507E\sua10 <sua100913@gmail.com>
2021-05-03 18:05:13 +0900

    스프링부트 테스트

---
+++ src/test/java/hosportal/HosportalApplicationTests.java
@@ -0,0 +1,13 @@
+package hosportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HosportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
