cc3bab3e49c5b463c40bf50d459459909c7f2f7a
DESKTOP-FK0507E\sua10 <sua100913@gmail.com>
2021-05-03 17:10:55 +0900

    커뮤니티 댓글 구성목록

---
+++ src/main/java/hosportal/dto/ReCommunityDto.java
@@ -0,0 +1,13 @@
+package hosportal.dto;
+
+import lombok.Data;
+
+@Data
+public class ReCommunityDto {
+
+	private int reCommunityNo;
+	private int commuNo;
+	private String reContents;
+	private String reCreatedDate;
+	private String nMemberId;
+}
